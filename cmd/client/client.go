cd7acc93b1e97d9bb0fdfff2624885999d052e5d
Victor Login <batazor@evrone.com>
2023-09-22 01:38:31 +0200

    cmd: add cmd for client/server

---
+++ cmd/client/client.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/batazor/word_of_wisdow/internal/pkg/logger"
+	"github.com/batazor/word_of_wisdow/internal/pkg/tcp"
+)
+
+func main() {
+	// Init context
+	_, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Init logger
+	log, err := logger.New()
+	if err != nil {
+		panic(err)
+	}
+
+	// Init client
+	uri := "localhost:8080"
+	client, err := tcp.NewClient(uri)
+	if err != nil {
+		panic(err)
+	}
+
+	// Send ping
+	_, err = client.Conn.Write([]byte("ping"))
+
+	// Read message
+	for msg := range client.ReadCh {
+		log.Info(fmt.Sprintf("Message: %s", msg))
+	}
+
+	// TODO: Graceful shutdown
+	return
+}
---
+++ cmd/server/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+
+	"github.com/batazor/word_of_wisdow/internal/pkg/logger"
+	"github.com/batazor/word_of_wisdow/internal/pkg/tcp"
+	"go.uber.org/zap"
+)
+
+func main() {
+	// Init context
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Init logger
+	logger, err := logger.New()
+	if err != nil {
+		panic(err)
+	}
+
+	// Init server
+	uri := "localhost:8080"
+	server, err := tcp.NewServer(ctx, uri, logger)
+	if err != nil {
+		panic(err)
+	}
+
+	// read data from the server
+	for msg := range server.ReadCh {
+		logger.Info("msg", zap.String("msg", string(msg)))
+		server.Send([]byte("pong\n"))
+	}
+
+	// Graceful shutdown
+	return
+}
